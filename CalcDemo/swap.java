5f685878f409f3cfa90c41f4f4fe32b90f7926db
Ashvitha Thathvam <ashwitha.t31@gmail.com>
2023-11-23 12:52:54 +0000

    New commit

---
+++ CalcDemo/swap.java
@@ -0,0 +1,24 @@
+package CalcDemo;
+
+public class swap {
+    public static void main(String[] args) {
+
+        int A = 20;
+        int B = 30;
+        System.out.println("Before swapping two numbers "+A+" "+B);
+
+        int C = A;
+        A = B;
+        B = C;
+
+        
+    
+        
+        System.out.println("After swapping two numbers "+A+" "+B);
+        
+    }
+    
+}
+
+    
+
